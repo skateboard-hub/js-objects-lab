b32f96507d8b44e46c17f4ef4909995bb3f47251
Shunyi Xu <3051778755@qq.com>
2022-09-13 19:59:45 +0800

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -9,7 +9,12 @@ const aCar=
     date  : '201', 
     county   : 'WD', 
     number  : '1058'
- }
+ },
+ owner: "Joe Bloggs",
+ previous_owners: [
+    { name: "Cloter", address: "Chaoyang Street" },
+    { name: "David", address: "Lacken Road" }
+ ]
  };
  console.log(aCar.owner + ' drives a ' + aCar.type.make)
  console.log('Reg. = ' + aCar.registration.date+"-"+aCar.registration.county+"-"+aCar.registration.number)
@@ -20,3 +25,5 @@ const aCar=
     shade : 'red'
  };
 console.log('It is a ' + aCar.colour.shade+' car, ' + aCar.mileade + ' mileage, with ' + aCar.colour.texture + ' interior.');
+console.log("First owner: " +aCar.previous_owners[0].name +' - ' +aCar.previous_owners[0].address);
+console.log("First owner: " +aCar.previous_owners[1].name +' - ' +aCar.previous_owners[1].address);
