34a7cc9a7c4f0a5e99702c3c002eaebb3f41d80d
Shunyi Xu <3051778755@qq.com>
2022-09-13 19:09:20 +0800

    My properties.

--- fundamentals.js
+++ fundamentals.js
@@ -5,5 +5,18 @@ const aCar=
     Corolla   : 'Corolla', 
     CC  : '1.8'
  },
- registration : '201WD1058' };
+ registration  :{
+    date  : '201', 
+    county   : 'WD', 
+    number  : '1058'
+ }
+ };
  console.log(aCar.owner + ' drives a ' + aCar.type.make)
+ console.log('Reg. = ' + aCar.registration.date+"-"+aCar.registration.county+"-"+aCar.registration.number)
+
+ aCar.mileade = '10000';
+ aCar.colour = {
+    texture : 'leather',
+    shade : 'red'
+ };
+console.log('It is a ' + aCar.colour.shade+' car, ' + aCar.mileade + ' mileage, with ' + aCar.colour.texture + ' interior.');
