47a9c92a5a768c74e0dd9e302989d3ab841ee2a3
Shunyi Xu <3051778755@qq.com>
2022-09-13 18:01:14 +0800

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -1,4 +1,9 @@
 const aCar=
 { owner : "Joe Bloggs",
- type : 'Toyota Corolla 1.8', 
+  type  :{
+    make  : 'Toyota', 
+    Corolla   : 'Corolla', 
+    CC  : '1.8'
+ },
  registration : '201WD1058' };
+ console.log(aCar.owner + ' drives a ' + aCar.type.make)
